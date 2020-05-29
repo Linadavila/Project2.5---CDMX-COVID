cf4c9b00f6932d2cf2c6bc4d678fc94de8395beb
Ana Cris <arcosmrn@gmail.com>
2020-05-28 21:58:32 -0500

    Add files via upload

---
+++ static/data.js
@@ -0,0 +1,9 @@
+const url2 =  "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=capacidad-hospitalaria&q=&facet=fecha&facet=nombre_hospital&facet=institucion&facet=estatus_capacidad_hospitalaria&facet=estatus_capacidad_uci"
+d3.json(url2).then(function(data) {
+    console.log(data);
+});
+
+const url =  "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=casos-asociados-a-covid-19&q=&facet=fecha_actualizacion&facet=origen&facet=sector&facet=entidad_um&facet=sexo&facet=entidad_nac&facet=entidad_res&facet=municipio_res&facet=tipo_paciente&facet=fecha_ingreso&facet=fecha_def&facet=edad&facet=nacionalidad&facet=embarazo&facet=habla_lengua_indi&facet=diabetes&facet=epoc&facet=asma&facet=inmusupr&facet=hipertension&facet=cardiovascular&facet=obesidad&facet=renal_cronica&facet=tabaquismo&facet=resultado&facet=migrante&facet=pais_nacionalidad&facet=rango_edad&facet=geo_point_2d"
+d3.json(url).then(function(data) {
+    console.log(data);
+});
